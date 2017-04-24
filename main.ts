106b70143da7a29a3e334836d3e6e6f12ad26e5d
Oscar Busk <oscar.busk@silobreaker.com>
2017-04-24 18:51:29 +0200

    Export TcModule

--- main.ts
+++ main.ts
@@ -1,8 +1,39 @@
-export * from './lib/tc_exchange_db.service';
-export * from './lib/tc_group.service';
-export * from './lib/tc_indicator.service';
-export * from './lib/tc_owner.service';
-export * from './lib/tc_security_labels.service';
-export * from './lib/tc_utility.service';
+import { NgModule } from '@angular/core';
+
+import { TcExchangeDbService } from './lib/tc_exchange_db.service';
+import { TcGroupService } from './lib/tc_group.service';
+import { TcIndicatorService } from './lib/tc_indicator.service';
+import { TcOwnerService } from './lib/tc_owner.service';
+import { TcSecurityLabelService } from './lib/tc_security_labels.service';
+import { TcUtilityService } from './lib/tc_utility.service';
+
+// Third part
+import { GrowlModule } from 'primeng/primeng';
+
+@NgModule({
+    imports: [
+        GrowlModule,
+    ],
+    providers: [
+        TcExchangeDbService,
+        TcGroupService,
+        TcIndicatorService,
+        TcOwnerService,
+        TcSecurityLabelService,
+        TcUtilityService,
+    ]
+})
+class TcModule { }
+
+export {
+    TcExchangeDbService,
+    TcGroupService,
+    TcIndicatorService,
+    TcOwnerService,
+    TcSecurityLabelService,
+    TcUtilityService,
+    TcModule,
+};
+
 export * from './lib/tc_resource_type';
 export * from './lib/entities';
